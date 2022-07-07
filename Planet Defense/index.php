2ecd31640ddb9f68bf316d18df600ab2ac71ff87
UfukDemirel <85198386+UfukDemirel@users.noreply.github.com>
2022-07-07 12:30:52 +0300

    Planet Game

---
+++ Planet Defense/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<canvas id="canvas"></canvas>
+<a href="https://codepen.io/Loopez10/full/dMaVdQ" class="full-screen" target="_blank"></a>
+</body>
+
+<script src="script.js"></script>
+</html>
